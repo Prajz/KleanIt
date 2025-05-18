4938e71159964eeddf5d8a4f9dd0e0a895b35613
Prajz <dp820@bath.ac.uk>
2025-05-18 11:45:58 +0100

    Initial Commit

---
+++ KleanIt/KleanItApp.swift
@@ -0,0 +1,17 @@
+//
+//  KleanItApp.swift
+//  KleanIt
+//
+//  Created by Dev Prajapati on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct KleanItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
